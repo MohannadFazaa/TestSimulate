ce3fc822822cd885818c0131af70e1dd5ca4cb99
Druzhee <MOHANNADSA54@GMAIL.COM>
2025-02-02 13:35:32 +0100

    Add project files.

---
+++ TestSimulate/Pages/Index.cshtml.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TestSimulate.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        public string Message { get; set; } = "Ingen simulerad traffik utförd";
+        public async Task<IActionResult> OnPostMinKnapp()
+        {
+            var uri = "https://simulate-newton.azurewebsites.net";
+
+            using var client = new HttpClient();
+
+            for (int i = 0; i < 100; i++)
+            {
+                try
+                {
+                    var response = await client.GetAsync(uri);
+                    Console.WriteLine($"Request {i + 1}, status : {response.StatusCode}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Request {i + 1} Failed: {ex.Message}");
+                }
+                await Task.Delay(10);
+            }
+            Message = "simulerad traffik utförd";
+            return Page(); 
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
